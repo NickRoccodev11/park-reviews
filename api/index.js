f372260b82cbaebebdbe0eb32dbdb82d9b2a0ebe
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-01 13:59:54 -0400

    added index for api routes, complete with token verfier)

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+
+router.use((req, res, next) => {
+  const auth = req.headers.authorization;
+  const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
+  if (token) {
+    req.user = jwt.verify(token, process.env.JWT);
+  } else {
+    req.user = null;
+  }
+  next();
+});
+
+router.use("/auth", require("./auth"));
+router.use("/api", require("./api"));
+
+module.exports = router;
