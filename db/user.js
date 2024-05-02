e68750aa723658f85537de25530479196b7c601d
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-02 19:37:11 -0400

    db function for getReviewsByUser

--- db/user.js
+++ db/user.js
@@ -24,7 +24,6 @@ const registerUser = async (
   }
 };
 
-
 const loginUser = async (username, password) => {
   try {
     const loggedUser = await prisma.user.findFirst({
@@ -33,10 +32,23 @@ const loginUser = async (username, password) => {
         password,
       },
     });
-   return loggedUser
+    return loggedUser;
   } catch (error) {
-    console.error("error finding user", error)
+    console.error("error finding user", error);
   }
 };
 
-module.exports = { registerUser, loginUser };
+const getReviewsByUser = async (id) => {
+  try {
+    const userReviews = await prisma.review.findMany({
+      where: {
+        user_id: id,
+      },
+    });
+    return userReviews
+  } catch (error) {
+    console.error("error getting user reviews from db", error)
+  }
+};
+
+module.exports = { registerUser, loginUser, getReviewsByUser };
