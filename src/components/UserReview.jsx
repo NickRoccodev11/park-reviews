028aa04bcb807b94496b80eabc041ddc147c9391
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-04 06:43:26 -0400

    delete button and fetch for user reviews

--- src/components/UserReview.jsx
+++ src/components/UserReview.jsx
@@ -4,13 +4,32 @@ import ReviewUpdateForm from "./ReviewUpdateForm"
 const UserReview = ({ token, review, setUserReviews }) => {
   const [showUpdateForm, setShowUpdateForm] = useState(false)
 
+  const handleDelete = async (id) => {
+    const result = fetch(`/auth/reviews/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    const deletedReview = await result.json()
+    if (deletedReview.id) {
+      setUserReviews(prev => {
+        return prev.map(review => {
+          if (review.id !== deletedReview.id) {
+            return review
+          }
+        })
+      })
+    }
+  }
   return (
     <div className="user-review">
       <h3>{review.title}</h3>
       <h4>a review for {review.park.name}</h4>
       <p>{review.content}</p>
       <p>rating: {review.stars} out of 5 stars</p>
-      <button onClick={() => setShowUpdateForm(true)}>update review</button>
+      <button onClick={() => setShowUpdateForm(true)}>update review</button><br />
       {
         showUpdateForm &&
         <ReviewUpdateForm
@@ -20,6 +39,7 @@ const UserReview = ({ token, review, setUserReviews }) => {
           token={token}
         />
       }
+      <button onClick={() => handleDelete(review.id)}>delete review</button>
     </div>
   )
 }
