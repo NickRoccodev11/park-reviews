e759d7d75c56ad76cb539b11a5a9a140eb5bc6bd
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-05 21:52:01 -0400

    frontend delete fetch for comments

--- src/components/UserComment.jsx
+++ src/components/UserComment.jsx
@@ -34,11 +34,29 @@ const UserComment = ({ comment, setUserComments, token }) => {
     }
   }
 
+  const handleDelete = async () => {
+    try {
+      const result = await fetch(`/auth/comments/${comment.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${token}`
+        }
+      })
+      const deletedComment = await result.json()
+      setUserComments(prev => {
+        return prev.filter(comment => comment.id !== deletedComment.id)
+      })
+    } catch (error) {
+      console.error("error deleting comment in fetch", error)
+    }
+  }
+
   return (
     <div className='user-comment'>
       <h4>{comment.content}</h4>
       <span>comment made on the review: "{comment.review.title}"</span><br />
-      <button onClick={() => setShowEditForm(true)}>edit comment</button>
+      <button onClick={() => setShowEditForm(true)}>edit comment</button><br />
       {
         showEditForm &&
         <div>
@@ -53,6 +71,7 @@ const UserComment = ({ comment, setUserComments, token }) => {
           </form>
         </div>
       }
+      <button onClick={handleDelete}>delete comment</button>
     </div>
   )
 }
