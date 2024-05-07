8f0a389651458ec84db77f30f6a2ddd48b1e67a3
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-07 00:20:57 -0400

    average rating on cards

--- src/components/SinglePark.jsx
+++ src/components/SinglePark.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
+const getAverageRating = (reviews)=>{
+const total = reviews.reduce((acc,curr)=>acc+ curr.stars, 0 )
+const average = total/ reviews.length
+return average.toFixed(2)
+}
+
 const SinglePark = ({ park }) => {
   const navigate = useNavigate()
+  console.log(park)
   return (
     <div className='single-park'>
       <h3>{park.name}</h3>
       <img src={park.image} />
       <p>state: {park.state}</p>
-      <span>Tags: </span>
+      <span>tags: </span>
       {
-        park.Tag.length> 0 &&
+        park.Tag.length > 0 &&
 
-        park.Tag.map(tag=>{
-          return(
+        park.Tag.map(tag => {
+          return (
             <div className='tag-container'>
-            <span className='tag'> {tag.category}</span><br/>
+              <span className='tag'> {tag.category}</span><br />
             </div>
-          ) 
+          )
         })
       }
-      <button onClick={()=>navigate(`/${park.id}`)}>See Details</button>
+      {
+        park.Review.length > 0 &&
+        <p>average rating: {getAverageRating(park.Review)} </p>
+      }
+      <button onClick={() => navigate(`/${park.id}`)}>See Details</button>
     </div>
   );
 }
