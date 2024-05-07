d1d35c368aa85bcdd0ced327bb526bb5daa93039
NickRoccodev11 <nchls.rcc@gmail.com>
2024-05-06 20:23:15 -0400

    endpoint for createComment

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
-const { getAllParks, getParkDetails, createPark, createReview } = require("../db/park.js");
-
+const {
+  getAllParks,
+  getParkDetails,
+  createPark,
+  createReview,
+  createComment
+} = require("../db/park.js");
 
 router.get("/parks", async (_req, res) => {
-  
   try {
     const allParks = await getAllParks();
     res.status(200).send(allParks);
@@ -12,9 +16,7 @@ router.get("/parks", async (_req, res) => {
   }
 });
 
-
 router.get("/parks/:id", async (req, res) => {
-  
   try {
     const { id } = req.params;
     const parkDetails = await getParkDetails(id);
@@ -29,40 +31,54 @@ router.get("/parks/:id", async (req, res) => {
   }
 });
 
-router.post ("/parks", async (req, res) => {
+router.post("/parks", async (req, res) => {
   try {
-    const newPark= await createPark(
-      req.body.name, 
-      req.body.description, 
-      req.body.contact, 
-      req.body.state, 
-      req.body.image, 
+    const newPark = await createPark(
+      req.body.name,
+      req.body.description,
+      req.body.contact,
+      req.body.state,
+      req.body.image,
       req.body.hours
-    )
-    res.status(201).send(newPark); 
+    );
+    res.status(201).send(newPark);
   } catch (error) {
     console.log(error);
   }
 });
 
-router.post ("/reviews", async (req, res) =>{
+router.post("/reviews", async (req, res) => {
   if (req.user) {
     try {
       const newReview = await createReview(
-        req.body.title, 
-        req.body.content, 
-        req.body.stars, 
+        req.body.title,
+        req.body.content,
+        req.body.stars,
         req.user.id,
         req.body.park_id
-  
-      )
+      );
       res.status(201).send(newReview);
     } catch (error) {
       console.log(error);
     }
   }
-  });
+});
 
-  
+router.post("/comments", async (req, res) => {
+  if (req.user) {
+    try {
+      const newComment = await createComment(
+        req.body.content,
+        req.body.review_id,
+        req.user.id
+      );
+      res.status(200).send(newComment);
+    } catch (error) {
+      console.error("error on POST /comments route", error);
+    }
+  } else {
+    res.status(404).send({ msg: "you must be signed in to leave a comment" });
+  }
+});
 
 module.exports = router;
